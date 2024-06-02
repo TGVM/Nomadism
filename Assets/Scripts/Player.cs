282f87138f2daccd2a49c5b2d5e18f3e91312931
TGVM <tgvm96@gmail.com>
2024-06-02 16:38:25 -0300

    object collect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector3 newPosition;
-    [SerializeField] float speed = 5f;
+    private Vector3 movePosition;
+    private List<GameObject> allObjects;
 
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float range = 2f;
+    [SerializeField] private int inventory = 0;
+    [SerializeField] private int capacity = 5;
+    
 
     void Start()
     {
-        newPosition = transform.position;
-
+        movePosition = transform.position;
+        allObjects = GameObject.FindGameObjectsWithTag("Collectable").ToList();
         
     }
 
     void Update()
+    {
+        Movement();
+        CollectObject();
+    }
+
+
+    void Movement()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -23,13 +36,68 @@ public class Player : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                newPosition = hit.point;
-                newPosition.y = transform.position.y;
-
-                
+                movePosition = hit.point;
+                movePosition.y = transform.position.y;
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, movePosition, speed * Time.deltaTime);
 
     }
+
+    void CollectObject()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (GetNearestObjectDistance() < range)
+            {
+                if (inventory < capacity)
+                {
+                    GameObject target = GetNearestObject();
+                    target.SetActive(false);
+                    allObjects.Remove(target);
+
+                    inventory++;
+                }
+
+            }
+        }
+    }
+
+    GameObject GetNearestObject()
+    {
+        GameObject nearestObject = null;
+        float nearestDistance = float.MaxValue;
+
+        for (int i = 0; i < allObjects.Count; i++)
+        {
+            float distance = (allObjects[i].transform.position - transform.position).sqrMagnitude;
+
+            if (distance < nearestDistance)
+            {
+                nearestObject = allObjects[i];
+                nearestDistance = distance;
+            }
+        }
+
+        return nearestObject;
+    }
+
+    float GetNearestObjectDistance()
+    {
+        float nearestDistance = float.MaxValue;
+
+        for (int i = 0; i < allObjects.Count; i++)
+        {
+            float distance = (allObjects[i].transform.position - transform.position).sqrMagnitude;
+
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+            }
+        }
+
+        return nearestDistance;
+    }
+
+
 }
